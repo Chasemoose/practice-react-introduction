6c346ee8036aa725a6cb8a64e906ef88d67b4ab3
Jarek Stelmach <j.stelmach@onet.eu>
2024-07-11 23:12:54 +0200

    Wrote code for MenuItem.js file

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
+import MenuItem from './MenuItem'
 
 const root = createRoot(document.querySelector('#root'))
+
+const App = () => <div><MenuItem /></div>
+
+root.render(<App />)
 
