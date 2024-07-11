63ea1b86893d1a0f1c44f20c87dcac108f936fd3
Jarek Stelmach <j.stelmach@onet.eu>
2024-07-11 23:17:22 +0200

    Made correction to app.js

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import MenuItem from './MenuItem'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import MenuItem from "./MenuItem"
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"))
 
-const App = () => <div><MenuItem /></div>
+const App = () => (
+	<div>
+		<ul>
+			<MenuItem text='Kontakt' url='/contact' />
+			<MenuItem text='Home' url='/' />
+			<MenuItem text='O nas' url='/about' />
+		</ul>
+	</div>
+)
 
 root.render(<App />)
-
