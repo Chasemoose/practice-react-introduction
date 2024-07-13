87a3176805fd938f3a1b882f250de67d198b106d
Jarek Stelmach <j.stelmach@onet.eu>
2024-07-14 00:09:47 +0200

    Added some code to Menu.js file

---
+++ 05/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import MenuItem from './MenuItem'
+
+const MenuItem = ({items}) => {
+    <ul>
+        {items.map((item) => (
+            
+        ))}
+    </ul>
+}
